da8d7aca6f4de6634288276577c9b74941d7699e
Songmu <y.songmu@gmail.com>
2014-12-06 23:53:38 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+TravisExperiment - It's new $module
+
+# SYNOPSIS
+
+    use TravisExperiment;
+
+# DESCRIPTION
+
+TravisExperiment is ...
+
+# LICENSE
+
+Copyright (C) Songmu.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Songmu <y.songmu@gmail.com>
---
+++ lib/TravisExperiment.pm
@@ -0,0 +1,39 @@
+package TravisExperiment;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+TravisExperiment - It's new $module
+
+=head1 SYNOPSIS
+
+    use TravisExperiment;
+
+=head1 DESCRIPTION
+
+TravisExperiment is ...
+
+=head1 LICENSE
+
+Copyright (C) Songmu.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Songmu E<lt>y.songmu@gmail.comE<gt>
+
+=cut
+
